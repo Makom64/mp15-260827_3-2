4f68321c2851f2312d872e5a1608df71fc82823f
Makom64 <yellowffd400@gmail.com>
2026-08-28 08:49:59 +0900

    [Feat] 저장소 준비

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== N조 팀원 소개 ===");
+
+
+        Console.WriteLine("=== 소개 끝 ===");
+    }
+    // 자기소개 메서드 추가 부탁드립니다.
+    
+}
